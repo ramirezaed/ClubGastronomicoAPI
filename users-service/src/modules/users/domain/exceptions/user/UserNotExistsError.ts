c00ca2489493e631a5e834a9f00c9c0368261730
ramirezaed <102742026+ramirezaed@users.noreply.github.com>
2026-04-11 13:03:07 -0300

    Update users-service/src/modules/users/domain/exceptions/user/UserNotExistsError.ts
    Co-authored-by: Carlos Alberto Romero <cromero2386@gmail.com>

--- users-service/src/modules/users/domain/exceptions/user/UserNotExistsError.ts
+++ users-service/src/modules/users/domain/exceptions/user/UserNotExistsError.ts
@@ -1,6 +1,6 @@
 export class UserNotExistError extends Error {
   constructor() {
-    super(`El usuario que buscas no existe o fue eliminado.`);
+    super(`El usuario que buscas no existe.`);
     this.name = "UserNotExistError";
   }
 }
