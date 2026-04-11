1ada568c5de4f77f5acab7332e273b7af55a522b
ramirezaed <102742026+ramirezaed@users.noreply.github.com>
2026-04-11 13:03:41 -0300

    Update users-service/src/modules/users/domain/exceptions/role/RolesNotFoundError.ts
    Co-authored-by: Carlos Alberto Romero <cromero2386@gmail.com>

--- users-service/src/modules/users/domain/exceptions/role/RolesNotFoundError.ts
+++ users-service/src/modules/users/domain/exceptions/role/RolesNotFoundError.ts
@@ -1,7 +1,7 @@
 //clase con error perzonalizado para evitar el duplicado de roles
 export class RolesNotFoundError extends Error {
   constructor() {
-    super(`No se encontro el rol`);
+    super(`No contamos con datos del roll`);
     this.name = "RolesNotFoundError";
   }
 }
