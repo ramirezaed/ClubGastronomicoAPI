932f812c764bf4fe8b1ddbfa06bc1dfcdd938088
ramirezaed <102742026+ramirezaed@users.noreply.github.com>
2026-04-11 13:03:54 -0300

    Update users-service/src/modules/users/domain/exceptions/user/UpdateRoleUserError.ts
    Co-authored-by: Carlos Alberto Romero <cromero2386@gmail.com>

--- users-service/src/modules/users/domain/exceptions/user/UpdateRoleUserError.ts
+++ users-service/src/modules/users/domain/exceptions/user/UpdateRoleUserError.ts
@@ -1,6 +1,6 @@
 export class UpdateRoleUserError extends Error {
   constructor() {
-    super(`Ocurrio un error al cambiar el rol del usuario.`);
+    super(`Ocurrio un error al intentar cambiar el rol del usuario.`);
     this.name = "ChangeRoleUserError";
   }
 }
