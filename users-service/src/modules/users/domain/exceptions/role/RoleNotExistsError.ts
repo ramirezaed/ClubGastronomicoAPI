e7508d3517254ba5a8528503c74b8d6aff466937
ramirezaed <102742026+ramirezaed@users.noreply.github.com>
2026-04-11 13:03:28 -0300

    Update users-service/src/modules/users/domain/exceptions/role/RoleNotExistsError.ts
    Co-authored-by: Carlos Alberto Romero <cromero2386@gmail.com>

--- users-service/src/modules/users/domain/exceptions/role/RoleNotExistsError.ts
+++ users-service/src/modules/users/domain/exceptions/role/RoleNotExistsError.ts
@@ -1,6 +1,6 @@
 export class RoleNotExistsError extends Error {
   constructor() {
-    super(`el rol que buscas no se encuentra registrado, o fue eliminado`);
+    super(`el rol que buscas no se encuentra`);
     this.name = "RoleNotExistsError";
   }
 }
